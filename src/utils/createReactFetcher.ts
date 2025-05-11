7e37e1b9520d2097df31467fdaa90fc633e369ee
Nico Brinkkemper <nico@brinkkemper.nl>
2025-05-11 02:38:53 +0200

    try only with host

--- src/utils/createReactFetcher.ts
+++ src/utils/createReactFetcher.ts
@@ -5,7 +5,7 @@ import { callServer } from "./callServer.js";
 
 export function createReactFetcher({
   url = window.location.pathname,
-  moduleBaseURL = new URL(import.meta.env.BASE_URL, window.location.origin).toString().slice(0, -1),
+  moduleBaseURL = new URL(import.meta.env.BASE_URL, window.location.host).toString().slice(0, -1),
   headers = { Accept: "text/x-component" },
 }: {
   url?: string;
